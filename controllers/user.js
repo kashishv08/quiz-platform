512d580991afddca401a9877431e6d22610e3681
kashish Vashistha <155710948+kashishv08@users.noreply.github.com>
2025-05-04 16:47:41 +0530

    Update user.js
    changes in logout route

--- controllers/user.js
+++ controllers/user.js
@@ -31,7 +31,7 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
   req.logout(() => {
-    req.flash("success", "Goodbye!");
-    res.redirect("/quizzes");
+    req.flash("success", "Thanks for Visiting :)");
+    res.redirect("/");
   });
 };
